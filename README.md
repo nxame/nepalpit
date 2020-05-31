df9731156fdc578d6125df6b4ee7768e8e5d36ec
shekhardesigner <itsmes.sh@gmail.com>
2020-06-01 03:05:02 +0545

    Code Tooling added

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Nepal PIT
 
 Personal Income Tax (PIT) calculator utility for Nepal
+
+### Project Structure
+ * `src` - All source code
+ * `test` - Test codes
+ * `dist` - Compiled javascript code
+ * `docs` - Example codes (_docs_ name for ease to publish to github pages)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-/**
- * 
+/*!
+ *
  * @danfebooks/nepalpit
  * Personal Income Tax (PIT) calculator utility for Nepal
  * https://www.danfebooks.com/calculators/personal-income-tax
- * 
- * 
+ *
+ *
  * MIT License
  * Copyright (c) 2020 danfebooks™
- * 
+ *
  * Index.js
  */
 
+export default function tax() {
+	return true;
+}
